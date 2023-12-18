c09f921f75bedc381a9288b83c7d1da260e214a1
Jake Elsberry <jelsberry96@gmail.com>
2023-12-18 09:10:59 -0800

    calendar refactor resume - possibly missing plugin

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 function Calendar() {
 
+  let calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek'
+    }
+  });
+
   return (
     <React.Fragment>
- 
-
-
-
+      <h3>{calendar}</h3>
     </React.Fragment>
   )
 
