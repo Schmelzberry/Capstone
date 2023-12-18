1ef390c6f0732310325a9cb0c55f3aa4050c2977
Jake Elsberry <jelsberry96@gmail.com>
2023-12-18 08:32:32 -0800

    calendar refactor resume

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,22 +5,7 @@ function Calendar() {
   return (
     <React.Fragment>
  
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
-    <script>
 
-      document.addEventListener('DOMContentLoaded', function() {
-        let calendarEl = document.getElementById('calendar');
-        let calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth',
-        });
-        calendar.render();
-      });
-
-    </script>
-  
-  <body>
-    <div id='calendar'></div>
-  </body>
 
 
     </React.Fragment>
