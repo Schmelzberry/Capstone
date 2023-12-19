7d1405a67f042a3a5c0172e71ca3fc9167558a9e
Jake Elsberry <jelsberry96@gmail.com>
2023-12-19 10:02:43 -0800

    add React Router and sign in page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import ViewControl from "./ViewControl";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
+    <Router>
     <React.Fragment>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />}
       <Header />
       <ViewControl />
+      </Routes>
     </React.Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SignIn(){
+  return (
+    <h1>Sign-in too see our list of artists</h1>
+  );
+}
+
+export default SignIn
