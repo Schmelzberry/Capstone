bf872a914be06aa356fd8b01c807e28bab1de420
Jake Elsberry <jelsberry96@gmail.com>
2023-12-20 10:27:18 -0800

    user component WIP

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,67 @@
-import React from "react";
-import { auth } from './../firebase.js';
+import React, { useEffect, useState } from "react";
+import { auth, firestore } from './../firebase.js';
 
+function User() {
+  const [userInfo, setUserInfo] = useState(null);
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (auth.currentUser) {
+        const userId = auth.currentUser.uid;
+        try {
+          const userDoc = await firestore.collection("users").doc(userId).get();
+          if (userDoc.exists) {
+            setUserInfo(userDoc.data());
+          } else {
+            console.log("User document not found");
+          }
+        } catch (error) {
+          console.error("Error fetching user data", error);
+        }
+      }
+    };
 
-function User(){
+    fetchUserData();
+  }, []);
 
-  return(
-
+  return (
     <React.Fragment>
-      <h3>Username: {auth.UserInfo.displayName}</h3>
-      <h3>Email: {auth.UserInfo.email}</h3>
-      <h3>Photo: {auth.UserInfo.photoURL}</h3>
-      <h3>UserId: {auth.UserInfo.uid}</h3>
+      {userInfo ? (
+        <div>
+          <h3>Username: {userInfo.displayName}</h3>
+          <h3>Email: {userInfo.email}</h3>
+          <h3>Photo: {userInfo.photoURL}</h3>
+          <h3>UserId: {userInfo.uid}</h3>
+        </div>
+      ) : (
+        <p>Loading user data...</p>
+      )}
     </React.Fragment>
-  )
+  );
 }
 
 export default User;
+
+
+
+
+
+// import React from "react";
+// import { auth } from './../firebase.js';
+
+
+
+// function User(){
+
+//   return(
+
+//     <React.Fragment>
+//       <h3>Username: {auth.UserInfo.displayName}</h3>
+//       <h3>Email: {auth.UserInfo.email}</h3>
+//       <h3>Photo: {auth.UserInfo.photoURL}</h3>
+//       <h3>UserId: {auth.UserInfo.uid}</h3>
+//     </React.Fragment>
+//   )
+// }
+
+// export default User;
